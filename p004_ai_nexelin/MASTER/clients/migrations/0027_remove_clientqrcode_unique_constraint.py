c75f735279819c9e6c2ca615f2ec2077bac6fe73
ChuprinaDaria <chuprina.dariia@gmail.com>
2025-11-14 14:41:49 +0100

    Revert migration 0027 to depend on 0026 - requires docker rebuild

--- p004_ai_nexelin/MASTER/clients/migrations/0027_remove_clientqrcode_unique_constraint.py
+++ p004_ai_nexelin/MASTER/clients/migrations/0027_remove_clientqrcode_unique_constraint.py
@@ -4,12 +4,10 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('clients', '0025_add_llm_provider_fk'),
+        ('clients', '0026_remove_client_llm_provider_model_and_more'),
     ]
 
     operations = [
-        # Try to remove unique_together constraint if it exists
-        # This will work regardless of whether 0026 was applied or not
         migrations.AlterUniqueTogether(
             name='clientqrcode',
             unique_together=set(),
