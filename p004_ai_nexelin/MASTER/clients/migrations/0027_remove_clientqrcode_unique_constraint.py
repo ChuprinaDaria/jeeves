19c28e69e53dcb2bad5a9f42b7fe7433f0270829
ChuprinaDaria <chuprina.dariia@gmail.com>
2025-11-14 14:40:17 +0100

    Fix migration 0027 dependency to work with servers missing 0026

--- p004_ai_nexelin/MASTER/clients/migrations/0027_remove_clientqrcode_unique_constraint.py
+++ p004_ai_nexelin/MASTER/clients/migrations/0027_remove_clientqrcode_unique_constraint.py
@@ -4,10 +4,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('clients', '0026_remove_client_llm_provider_model_and_more'),
+        ('clients', '0025_add_llm_provider_fk'),
     ]
 
     operations = [
+        # Try to remove unique_together constraint if it exists
+        # This will work regardless of whether 0026 was applied or not
         migrations.AlterUniqueTogether(
             name='clientqrcode',
             unique_together=set(),
