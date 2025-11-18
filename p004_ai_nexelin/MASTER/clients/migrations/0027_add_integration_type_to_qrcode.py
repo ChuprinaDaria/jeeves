c4a4b71c09871f0f0a99b937f8b60a1d2eb9683b
ChuprinaDaria <chuprina.dariia@gmail.com>
2025-11-18 12:47:58 +0000

    Fix migrations: merge conflicts, add QRCode integration_type and WebParsingRequest models

--- p004_ai_nexelin/MASTER/clients/migrations/0027_add_integration_type_to_qrcode.py
+++ p004_ai_nexelin/MASTER/clients/migrations/0027_add_integration_type_to_qrcode.py
@@ -7,16 +7,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('clients', '0026_remove_client_llm_provider_model_and_more'),
+        ('clients', '0025_add_llm_provider_fk'),
     ]
-
     operations = [
         migrations.AddField(
             model_name='clientqrcode',
             name='integration_type',
             field=models.CharField(
                 choices=[('whatsapp', 'WhatsApp'), ('web', 'Web Chat')],
-                default='whatsapp',
+                default='web',
                 help_text='Type of integration this QR code is for',
                 max_length=20,
                 verbose_name='Integration Type'
